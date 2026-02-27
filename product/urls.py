88cdfd55208d03bb2ccbff3e774b999efcc2c613
Kanykey <kanykey0823@icloud.com>
2026-02-27 21:36:42 +0600

    Initial commit - shop_api project

---
+++ product/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import (
+    CategoryListAPIView, CategoryDetailAPIView,
+    ProductListAPIView, ProductDetailAPIView,
+    ReviewListAPIView, ReviewDetailAPIView
+)
+
+urlpatterns = [
+    path('api/v1/categories/', CategoryListAPIView.as_view()),
+    path('api/v1/categories/<int:pk>/', CategoryDetailAPIView.as_view()),
+
+    path('api/v1/products/', ProductListAPIView.as_view()),
+    path('api/v1/products/<int:pk>/', ProductDetailAPIView.as_view()),
+
+    path('api/v1/reviews/', ReviewListAPIView.as_view()),
+    path('api/v1/reviews/<int:pk>/', ReviewDetailAPIView.as_view()),
+]
---
+++ product/views.py
@@ -0,0 +1,36 @@
+from rest_framework import generics
+from .models import Category, Product, Review
+from .serializers import CategorySerializer, ProductSerializer, ReviewSerializer
+
+
+# Categories
+class CategoryListAPIView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryDetailAPIView(generics.RetrieveAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+# Products
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+# Reviews
+class ReviewListAPIView(generics.ListAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+
+class ReviewDetailAPIView(generics.RetrieveAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
