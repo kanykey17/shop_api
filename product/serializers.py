eb1831091fb37d6ab172ae0c2e14a216700a7e71
Kanykey <kanykey0823@icloud.com>
2026-03-10 19:50:24 +0600

    Homework 4 validation

--- product/serializers.py
+++ product/serializers.py
@@ -3,16 +3,50 @@ from django.db.models import Avg
 from .models import Category, Product, Review
 
 
-class ReviewSerializer(serializers.ModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
+
     class Meta:
-        model = Review
-        fields = '__all__'
+        model = Category
+        fields = ['id', 'name']
+
+    def validate_name(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Category name must be at least 3 characters")
+        return value
 
 
 class ProductSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ['id', 'title', 'description', 'price', 'category']
+
+    def validate_title(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Title must be at least 3 characters")
+        return value
+
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Price must be greater than 0")
+        return value
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Review
+        fields = ['id', 'text', 'stars', 'product']
+
+    def validate_text(self, value):
+        if len(value) < 5:
+            raise serializers.ValidationError("Review text too short")
+        return value
+
+    def validate_stars(self, value):
+        if value < 1 or value > 5:
+            raise serializers.ValidationError("Stars must be between 1 and 5")
+        return value
 
 
 class ProductReviewSerializer(serializers.ModelSerializer):
@@ -26,14 +60,3 @@ class ProductReviewSerializer(serializers.ModelSerializer):
     def get_rating(self, obj):
         avg = obj.reviews.aggregate(Avg('stars'))['stars__avg']
         return round(avg, 2) if avg else 0
-
-
-class CategorySerializer(serializers.ModelSerializer):
-    products_count = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Category
-        fields = ['id', 'name', 'products_count']
-
-    def get_products_count(self, obj):
-        return obj.products.count()
